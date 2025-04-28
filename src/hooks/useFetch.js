950750dfd9e29f35a3261d40bf8a26f266c58d57
Oskar Blomberg <blomberg.oskar@gmail.com>
2025-04-28 09:50:59 +0200

    use-fetch hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+
+export default function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    setIsLoading(true);
+    setIsError(false);
+
+    axios
+      .get(url, { signal })
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        if (axios.isCancel(error)) {
+          console.log("Fetch request cancelled", error.message);
+        } else {
+          setIsError(true);
+        }
+      })
+      .finally(() => setIsLoading(false));
+
+    return () => controller.abort();
+  }, [url]);
+
+  return { data, isLoading, isError };
+}
