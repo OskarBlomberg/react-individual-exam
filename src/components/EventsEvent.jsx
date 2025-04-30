a49be670e0866100341d43082c1176ff47eb7c72
Oskar Blomberg <blomberg.oskar@gmail.com>
2025-04-30 15:52:24 +0200

    style links on event list page

--- src/components/EventsEvent.jsx
+++ src/components/EventsEvent.jsx
@@ -1,25 +1,29 @@
+import { Link } from "react-router-dom";
+
 export default function EventsEvent({
-  eventInfo: { name, where, when, price },
+  eventInfo: { name, where, when, price, id },
 }) {
   const dateArr = when.date.split(" ");
   const month = dateArr[1].slice(0, 3).toUpperCase();
 
   return (
-    <article className="event">
-      <div className="event__date">
-        <span>{dateArr[0]}</span>
-        <span>{month}</span>
-      </div>
-      <div className="event__container">
-        <h2 className="event__name">{name}</h2>
-        <span className="event__where">{where}</span>
-        <div className="event__time-price-container">
-          <time className="event__time">
-            {when.from} - {when.to}
-          </time>
-          <span className="event__price">{price} sek</span>
+    <Link to={`/events/${id}`} className="event-link">
+      <article className="event">
+        <div className="event__date">
+          <span>{dateArr[0]}</span>
+          <span>{month}</span>
         </div>
-      </div>
-    </article>
+        <div className="event__container">
+          <h2 className="event__name">{name}</h2>
+          <span className="event__where">{where}</span>
+          <div className="event__time-price-container">
+            <time className="event__time">
+              {when.from} - {when.to}
+            </time>
+            <span className="event__price">{price} sek</span>
+          </div>
+        </div>
+      </article>
+    </Link>
   );
 }
